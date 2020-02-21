369dd74eabe92871a03db7f8c24cc1bbc883f8f0
Dmboucher242 <58062008+Dmboucher242@users.noreply.github.com>
2020-02-20 21:51:05 -0700

    adding manifest and service worker

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const compression = require("compression");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
@@ -7,15 +9,18 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(compression());
+app.use(morgan("dev"));
 
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
-// routes
+
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
